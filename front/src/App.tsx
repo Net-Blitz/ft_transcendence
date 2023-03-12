17d4c2a95357342242cdc4f5c6d10264bbf1a565
genouf <gabriel.enouf@yahoo.fr>
2023-03-12 13:27:50 +0100

    [BUILD]f: new docker front_test

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import Hello from './components/Hello';
-import PrivateRoute from './components/PrivateRoute';
-import Login from './components/Login';
-import Dashboard from './components/Dashboard';
-import SearchUser from './components/SearchUser';
-import DoubleAuth from './components/DoubleAuth';
-import Login2fa from './components/Login2fa';
+import logo from './logo.svg';
+import './App.css';
 
-function App(this: any) {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/login" element={<Login />} />
-				<Route path="/login/2fa" element={<Login2fa />} />
-				<Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
-				<Route path="/" element={<PrivateRoute><Hello /></PrivateRoute>} />
-				<Route path="/search" element={<PrivateRoute><SearchUser /></PrivateRoute>} />
-				<Route path="/2fa" element={<PrivateRoute><DoubleAuth /></PrivateRoute>} />
-			</Routes>
-		</BrowserRouter>
-	);
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
