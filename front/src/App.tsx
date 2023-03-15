5af8bc7d50965ae5903ed8116800f3604f7adbc9
genouf <gabriel.enouf@yahoo.fr>
2023-03-15 17:00:19 +0100

    [ADD]f: LOGIN refacto routes

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import {Routes, Route} from "react-router-dom"
-import { LoginStart, Login2fa } from './Login/Login';
+import Login from './Login/Login';
 
 function App(this: any) {
 	return (
 		<Routes>
-			<Route path='/' element={<LoginStart/>}/>
-			<Route path='/2fa' element={<Login2fa/>}/>
+			{/*	LOGIN	*/}
+			<Route path='/start' element={<Login/>} />
+			<Route path='/2fa' element={<Login/>} />
 		</Routes>
 	);
 }
