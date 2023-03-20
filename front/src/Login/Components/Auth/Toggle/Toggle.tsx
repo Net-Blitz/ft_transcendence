7c5b1780bbf3945fa91ee4364298ac34a22813e9
genouf <gabriel.enouf@yahoo.fr>
2023-03-20 13:04:45 +0100

    [ADD]f: LOGIN toggle v1

--- front/src/Login/Components/Auth/Toggle/Toggle.tsx
+++ front/src/Login/Components/Auth/Toggle/Toggle.tsx
@@ -3,18 +3,46 @@ import './Toggle.css'
 
 const Toggle = () => {
 	/*	HOOK settings	*/
-	const [buttonState, setButtonState] = useState(1);
+	const [buttonState, setButtonState] = useState(true);
+	const [firstElement, setFirstElement] = useState('No');
+	const [secondElement, setSecondElement] = useState('Yes');
 
-  return (
-	<div className='toggle-wrapper'>
-		<div id='button-container' className='button'>
-			<div id='my_button' className='button-element'>
-				<p id='no'>No</p>
+	function toggleButton (event: any) {
+		const part_one = document.querySelector<HTMLElement>('.toggle-wrapper .button-element');
+		const part_two = document.querySelector<HTMLElement>('.toggle-wrapper #yes');
+
+		if (buttonState)
+		{
+			setButtonState(false);
+			setFirstElement('Yes');
+			setSecondElement('No');
+			if (part_one)
+				part_one.style.transform = 'translateX(100px)';
+			if (part_two)
+				part_two.style.transform = 'translateX(-90px)';
+		}
+		else
+		{
+			setButtonState(true);
+			setFirstElement('No');
+			setSecondElement('Yes');
+			if (part_one)
+				part_one.style.transform = 'translateX(0px)';
+			if (part_two)
+			part_two.style.transform = 'translateX(0px)';
+		}
+	} 
+
+	return (
+		<div className='toggle-wrapper'>
+			<div id='button-container' className='button' onClick={toggleButton}>
+				<div id='my_button' className='button-element'>
+					<p id='no'>{firstElement}</p>
+				</div>
+				<p id='yes'>{secondElement}</p>
 			</div>
-			<p id='yes'>Yes</p>
 		</div>
-	</div>
-  );
+	);
 }
 
 export default Toggle;
