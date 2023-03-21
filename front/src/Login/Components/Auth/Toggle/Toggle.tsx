7d3e756424b1a681d39c381cc7d6865442cf9107
Joséphine <j_bach@etu-webschoolfactory.fr>
2023-03-21 17:48:20 +0100

    [ADD]f: <Login> handle resize with the toggle button

--- front/src/Login/Components/Auth/Toggle/Toggle.tsx
+++ front/src/Login/Components/Auth/Toggle/Toggle.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import './Toggle.css'
 
 interface ToggleProps {
@@ -10,12 +10,10 @@ const Toggle = ({statusState, setStatusState} : ToggleProps) => {
 	/*	HOOK settings	*/
 	const [firstElement, setFirstElement] = useState('No');
 	const [secondElement, setSecondElement] = useState('Yes');
-
 	function toggleButton (event: any) {
 		const part_one = document.querySelector<HTMLElement>('.toggle-wrapper .button-element');
 		const part_two = document.querySelector<HTMLElement>('.toggle-wrapper #yes');
 		const screen_size: number = window.innerWidth;
-
 		if (!statusState)
 		{
 			setStatusState(true);
@@ -24,16 +22,16 @@ const Toggle = ({statusState, setStatusState} : ToggleProps) => {
 			if (part_one)
 			{
 				if (screen_size > 820)
-					part_one.style.transform = 'translateX(100px)';
+				part_one.style.transform = 'translateX(100px)';
 				else
-					part_one.style.transform = 'translateX(75px)';
+				part_one.style.transform = 'translateX(75px)';
 			}
 			if (part_two)
 			{
 				if (screen_size > 820)
-					part_two.style.transform = 'translateX(-90px)';
+				part_two.style.transform = 'translateX(-90px)';
 				else
-					part_two.style.transform = 'translateX(-63px)';
+				part_two.style.transform = 'translateX(-63px)';
 			}
 		}
 		else
@@ -42,12 +40,35 @@ const Toggle = ({statusState, setStatusState} : ToggleProps) => {
 			setFirstElement('No');
 			setSecondElement('Yes');
 			if (part_one)
-					part_one.style.transform = 'translateX(0px)';
+			part_one.style.transform = 'translateX(0px)';
 			if (part_two)
-				part_two.style.transform = 'translateX(0px)';
+			part_two.style.transform = 'translateX(0px)';
 		}
 	} 
-
+	
+	useEffect(() => {
+		const handleResize = () => {
+		  // reset the toggle state and styles when the window size changes
+		  setStatusState(false);
+		  setFirstElement('No');
+		  setSecondElement('Yes');
+		  const part_one = document.querySelector<HTMLElement>(
+			'.toggle-wrapper .button-element'
+		  );
+		  const part_two = document.querySelector<HTMLElement>(
+			'.toggle-wrapper #yes'
+		  );
+		  if (part_one) 
+			part_one.style.transform = 'translateX(0px)';
+		  if (part_two) 
+		  	part_two.style.transform = 'translateX(0px)';
+		};
+		window.addEventListener('resize', handleResize);
+		return () => {
+		  window.removeEventListener('resize', handleResize);
+		};
+	  }, []);
+	
 	return (
 		<div className='toggle-wrapper'>
 			<div id='button-container' className='button' onClick={toggleButton}>
