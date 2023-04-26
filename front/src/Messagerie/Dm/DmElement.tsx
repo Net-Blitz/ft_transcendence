393f3951cca000a76b6a6230f57302753dba0c21
genouf <gabriel.enouf@yahoo.fr>
2023-04-26 15:26:46 +0200

    [ADD]:f <chat> NewDm front ok

--- front/src/Messagerie/Dm/DmElement.tsx
+++ front/src/Messagerie/Dm/DmElement.tsx
@@ -1,21 +1,46 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import './DmElement.css';
 /*	Components	*/
 import { PopUp } from '../../Profile/Components/MainInfo/MainInfo';
 /*	Ressources	*/
 import close from '../../Profile/Components/MainInfo/Ressources/close.svg';
+import search from './Ressources/search.svg';
 
-const NewDm = () => {
+const InputFlat = ({ icon, content }: { icon: string; content: string }) => {
+	return (
+		<div className="input-flat">
+			<img src={icon} alt="search icon" />
+			<input type="text" placeholder={content} />
+		</div>
+	);
+};
+
+const NewDm = ({ handleNewDmTrigger }: { handleNewDmTrigger: () => void }) => {
+	const me = document.getElementsByClassName('popup');
+
+	useEffect(() => {
+		window.onclick = (event: any) => {
+			if (event.target === me[0]) {
+				handleNewDmTrigger();
+			}
+		};
+	}, [me, handleNewDmTrigger]);
+
 	return (
 		<div className="new-dm">
-			<img src={close} alt="close-button" />
-			<h3>NewDM</h3>
+			<img src={close} alt="close-button" onClick={handleNewDmTrigger} />
+			<h3>New DM</h3>
+			<InputFlat icon={search} content="Search a user" />
+			<div className="new-dm-buttons">
+				<button>Create</button>
+				<button onClick={handleNewDmTrigger}>Cancel</button>
+			</div>
 		</div>
 	);
 };
 
 const Aside = ({ buttonContent }: { buttonContent: string }) => {
-	const [newDmTrigger, setNewDmTrigger] = useState(true);
+	const [newDmTrigger, setNewDmTrigger] = useState(false);
 
 	const handleNewDmTrigger = useCallback(() => {
 		setNewDmTrigger(!newDmTrigger);
@@ -23,9 +48,11 @@ const Aside = ({ buttonContent }: { buttonContent: string }) => {
 
 	return (
 		<div className="dm-aside">
-			<button className="new-input">{buttonContent}</button>
+			<button className="new-input" onClick={handleNewDmTrigger}>
+				{buttonContent}
+			</button>
 			<PopUp trigger={newDmTrigger}>
-				<NewDm />
+				<NewDm handleNewDmTrigger={handleNewDmTrigger} />
 			</PopUp>
 		</div>
 	);
