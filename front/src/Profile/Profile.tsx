c9f803865aa6495caadb8f397e9aeaedb1c0c6fb
gkehren <kehren.guillaume@gmail.com>
2023-04-29 18:06:10 +0200

    [ADD]f: Logout button

--- front/src/Profile/Profile.tsx
+++ front/src/Profile/Profile.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import './Profile.css';
 /*	COMPONENTS	*/
 import { MainInfo } from './Components/MainInfo/MainInfo';
@@ -6,9 +7,19 @@ import { MiddleInfo } from './Components/MiddleInfo/MiddleInfo';
 import { MatchHistory } from './Components/MatchHistory/MatchHistory';
 
 export const Profile = () => {
+	const handleLogout = async () => {
+		await axios.get('http://localhost:3333/users/logout', {
+			withCredentials: true,
+		});
+		window.location.reload();
+	};
+
 	return (
 		<div className="profile-wrapper">
 			<h1>My Profile</h1>
+			<button className="profile-logout" onClick={handleLogout}>
+				Logout
+			</button>
 			<div className="main-wrapper">
 				<MainInfo />
 				<MiddleInfo />
